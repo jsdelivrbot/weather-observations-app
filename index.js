e8a638136fe1c4202473d3c12523869000d46d53
Lauri Koskinen <lauri.koskinen2@metropolia.fi>
2018-02-25 17:21:44 +0200

    added link to list weather observations

--- index.js
+++ index.js
@@ -51,14 +51,16 @@ app.get('/', function(request, response) {
 	
 });
 
-app.get('/weather_observation', function (request, response) {
+app.get('/weather_observation/:location', function(request,response){
+	var location = request.params.location;
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		client.query('SELECT * FROM weather_observation', function(err, result) {
+		client.query("select * from weather_observation where location = $1 order by timestamp desc limit 20", [location],
+		function(err, result) {
 			done();
 			if (err) {
 				console.error(err); response.send("Error " + err);
-			} else {
-				response.render('pages/helloworld', {results: result.rows} );
+			} else { 
+				response.render('pages/list' ,{results: result.rows}); 
 			}
 		});
 	});
@@ -78,55 +80,6 @@ app.post('/weather_observation',function(request,response){
 			}
 		});
 		response.end("It worked!");
-	});
-});
-
-app.get('/weather_observation/:location', function(request,response){
-	var location = request.params.location;
-	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		client.query("select * from weather_observation where location = $1", [location],
-		function(err, result) {
-			done();
-			if (err) {
-				console.error(err); response.send("Error " + err);
-			} else { 
-				response.contentType("json");
-				response.end(JSON.stringify(result.rows)); 
-			}
-		});
-	});
-});
-
-app.get('/weather_observation/:location/info', function(request,response){
-	var location = request.params.location;
-	var info = {}
-	//couldn't get min, max and latest temperature in one query so I made two.
-	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		client.query("select max(temperature), min(temperature) from weather_observation where location = $1 AND timestamp >= now()::date + interval '1h';", [location]
-		, function(err, result){
-			done();
-			if (err) {
-				console.error(err); response.send("Error " + err); 
-			} else {
-				info.min = result.rows[0].min;
-				info.max = result.rows[0].max;
-			}
-		});
-		client.query("select temperature from weather_observation where location = $1 order by timestamp desc limit 1;", [location]
-		, function(err, result){
-			done();
-			if (err) {
-				console.error(err); response.send("Error " + err); 
-			} else {
-				if(result.rowCount!=1){
-					info.latest = null;
-				} else {
-					info.latest = result.rows[0].temperature;
-				}
-				response.contentType("json");
-				response.end(JSON.stringify(info));
-			}
-		});
 	});
 });
 
